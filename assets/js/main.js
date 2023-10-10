c148a36305c9eade2a95b0c1f9674d6c99f754d2
Juliao F. <juliao.gf@gmail.com>
2023-10-10 06:15:55 -0300

    feat: Adicionado botão para carregar mais pokemons, até o limite definido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,41 @@
 const pokemonList = document.getElementById('pokemon-list')
+const loadMoreButton = document.getElementById('load-more')
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
+const maxRecords = 151
+const limit = 6
+let offset = 0
 
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
 
-            <img src="${pokemon.picture}" alt="${pokemon.name}">
-        </div>
-        </li>
-    `
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.picture}" alt="${pokemon.name}">
+            </div>
+            </li>
+        `).join('')
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordNextPage = offset + limit
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItems(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItems(offset, limit)
+    }
 })
+
+loadPokemonItems(offset, limit)
